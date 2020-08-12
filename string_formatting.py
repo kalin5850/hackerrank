18d852cc08fb90f5b4878853adf2bd46c528f659
kail.chang <kail.chang@aiacademy.tw>
2020-08-12 18:04:50 +0800

    add string formatting

---
+++ string_formatting.py
@@ -0,0 +1,17 @@
+from baseConvert import base_converter
+
+if __name__ == '__main__':
+
+    input = input()
+    BIN = 2
+    OCT = 8
+    HEX = 16
+
+
+    i = 1
+    while i <= 17:
+        String1 = "{}\t{}\t{}\t{}".format(i, base_converter(i, OCT), base_converter(i, HEX), base_converter(i, BIN))
+        print(String1)
+        i += 1
+
+
